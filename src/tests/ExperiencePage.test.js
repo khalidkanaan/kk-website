be7ade1942d593a086462ce7e531779123696709
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2024-10-12 23:58:56 -0400

    Updated unit tests to work with the new components and structure, updated  to include routes for , , and  using placeholder components for now

--- src/tests/ExperiencePage.test.js
+++ src/tests/ExperiencePage.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import ExperiencePage from '../components/ExperiencePage';
 
 test('renders DevOps Engineer heading', () => {
-  render(<ExperiencePage />);
+  render(
+    <MemoryRouter>
+      <ExperiencePage />
+    </MemoryRouter>);
   const headingElement = screen.getByText(/DevOps Engineer/i);
   expect(headingElement).toBeInTheDocument();
+
+  const logoElement = screen.getByText(/KHALIDKANAAN/i);
+  expect(logoElement).toBeInTheDocument();
+
+  const introLink = screen.getByText(/Introduction/i);
+  const experienceLink = screen.getByText(/Experience/i);
+  const projectsLink = screen.getByText(/Projects/i);
+  const educationLink = screen.getByText(/Education/i);
+
+  expect(introLink).toBeInTheDocument();
+  expect(experienceLink).toBeInTheDocument();
+  expect(projectsLink).toBeInTheDocument();
+  expect(educationLink).toBeInTheDocument();
 });
--- src/tests/HomePage.test.js
+++ src/tests/HomePage.test.js
@@ -1,8 +1,25 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from '../components/HomePage';
 
-test('renders Khalid Kana\'an heading', () => {
-  render(<HomePage />);
-  const headingElement = screen.getByText(/Khalid Kana'an/i);
-  expect(headingElement).toBeInTheDocument();
+test('renders the logo and navigation links', () => {
+  render(
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  );
+  
+  // Check if the logo is rendered
+  const logoElement = screen.getByText(/KHALIDKANAAN/i);
+  expect(logoElement).toBeInTheDocument();
+
+  const introLink = screen.getByText(/Introduction/i);
+  const experienceLink = screen.getByText(/Experience/i);
+  const projectsLink = screen.getByText(/Projects/i);
+  const educationLink = screen.getByText(/Education/i);
+
+  expect(introLink).toBeInTheDocument();
+  expect(experienceLink).toBeInTheDocument();
+  expect(projectsLink).toBeInTheDocument();
+  expect(educationLink).toBeInTheDocument();
 });
