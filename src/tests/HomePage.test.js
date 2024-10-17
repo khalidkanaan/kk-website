f0e0a8ce90b90aa73ad4bea53c21d0758e443253
Khalid Kanaan <khalid.kanaan.ca@gmail.com>
2024-10-17 00:01:01 -0400

    Refactored HomePage and App component tests to reflect new changes
    - modified tests in 'HomePage.test.js':
      - removed <MemoryRouter> as it is no longer required to render the HomePage
      - verified that left and right cards render correctly based on 'introData' content.
      - used a single render call for 'HomePage' across all tests.
      - checked for specific elements in the cards that have either title or description.
    
    - modified tests in 'App.test.js':
      - renamed 'checkNavigationLinks' helper function to 'checkHeaderElements' and included a check for the logo element.
      - added getByRole using link and the name instead of getByText to 'checkHeaderElements' helper to avoid errors where text appears more than once.
      - simplified 'HomePage' test by checking for a unique element (KhalidInDesert description) instead of redundant card checks, in App.test.js we are mainly testing the routing and we already test for everything in 'HomePage.test.js'
      - kept 'ExperiencePage' test unchanged as no modifications were made to that component yet.

--- src/tests/HomePage.test.js
+++ src/tests/HomePage.test.js
@@ -1,14 +1,34 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import HomePage from '../components/HomePage';
+import introData from '../assets/data/introData';
 
-test('renders name heading', () => {
-  render(
-    <MemoryRouter>
-      <HomePage />
-    </MemoryRouter>
-  );
-  
-  const homeHeading = screen.getByText(/Khalid Kana'an/i);
-  expect(homeHeading).toBeInTheDocument();
+jest.mock('../components/Card', () => ({ data }) => (
+  <div data-testid="mock-card">
+    {data.title || data.description || 'No title or description'}
+  </div>
+)); // Mock Card component
+
+describe('HomePage component', () => {
+  beforeEach(() => {
+    render(<HomePage />);
+  });
+
+  test('renders the left side with correct cards', () => {
+    expect(screen.getByText(introData.Welcome.title)).toBeInTheDocument();
+    expect(screen.getByText(introData.KhalidInDesert.description)).toBeInTheDocument();
+  });
+
+  test('renders the right side with correct cards', () => {
+    expect(screen.getByText(introData.AboutMe.title)).toBeInTheDocument();
+    expect(screen.getByText(introData.Links.title)).toBeInTheDocument();
+    expect(screen.getByText(introData.Tools.title)).toBeInTheDocument();
+    expect(screen.getByText(introData.KhalidCoding.description)).toBeInTheDocument();
+  });
+
+  test('renders the correct number of cards on both sides', () => {
+    const totalCards = 6;
+    const renderedCards = screen.getAllByTestId('mock-card');
+    expect(renderedCards).toHaveLength(totalCards);
+  });
 });
